0702243cd830887b4b8f276f95370a7b0ff2634e
hrjo0120 <hrjo0120@gmail.com>
2024-05-28 16:05:50 +0900

    Hello There

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Word1234!");
         System.out.println("sdf");
+        System.out.println("123");
     }
 }
